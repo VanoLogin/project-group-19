81ae33db879bf789e814edaa058e11b1989c662a
Kateryna Davidovych <kathylevchenko1@gmail.com>
2024-04-13 14:45:32 +0300

    update js
    test( not include in main.js yet)

---
+++ src/js/covers.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const coversSection = document.querySelector('.covers-section');
+  
+    const observerOptions = {
+      root: null, 
+      threshold: 0.5, 
+    };
+  
+     const observerCallback = (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.querySelectorAll('.animation-photo').forEach(photo => {
+            photo.style.animationPlayState = 'running';
+          });
+        } else {
+          entry.target.querySelectorAll('.animation-photo').forEach(photo => {
+            photo.style.animationPlayState = 'paused';
+          });
+        }
+      });
+    };
+  
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    observer.observe(coversSection);
+  });
+ 
