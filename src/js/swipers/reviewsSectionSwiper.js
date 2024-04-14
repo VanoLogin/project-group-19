2636fc3e6f684ac34f26f07406e1d19679378688
YaEhley <143018286+YaEhley@users.noreply.github.com>
2024-04-14 10:05:21 +0300

    correct swiper display

--- src/js/swipers/reviewsSectionSwiper.js
+++ src/js/swipers/reviewsSectionSwiper.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { reviewsTemplate } from "../templates/templateReviews"
 import Swiper from "swiper";
+import { Navigation, Keyboard} from 'swiper/modules';
 import 'swiper/css';
 
+let swiper;
 const reviewsListElem = document.querySelector('.swiper-wrapper');
 async function getReviews() {
     const url = 'https://portfolio-js.b.goit.study/api/reviews';
@@ -11,27 +13,53 @@ async function getReviews() {
 }
 
 async function init() {
-    const result = await getReviews();
-    const markUp = reviewsTemplate(result);
-    reviewsListElem.insertAdjacentHTML('afterbegin', markUp);
-    initSwiper()
+    try {
+        const result = await getReviews();
+        const markUp = reviewsTemplate(result);
+        reviewsListElem.insertAdjacentHTML('afterbegin', markUp);
+        initSwiper()
+    } catch {
+        // Change function
+        console.log('Error!')
+    }
+
 }
 init();
 
 
 function initSwiper() {
-    const swiper = new Swiper('.swiper', {
+    swiper = new Swiper('.swiper', {
+        modules: [Navigation, Keyboard],
+        keyboard: { enabled: true },
         // Default parameters
         slidesPerView: 1,
         spaceBetween: 16,
-        width: 343,
+        width:343,
         loop: false,
+
         
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
-
+        breakpoints: {
+            // 768: {
+            //     slidesPerView: 2,
+            // },
+            // // when window width is >= 640px
+            1440: {
+                // slidesPerView: 4,
+                width: 332,
+            }
+        }
     })
 }
 
+window.addEventListener('keydown', callBack)
+
+function callBack(e) {
+    if (e.key == 'Tab') {
+        console.log('hello');
+        swiper.slideNext();
+    }
+}
