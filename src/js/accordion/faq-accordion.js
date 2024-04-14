1ad246b91eebe8d98ff24cb252939ba1e089ae95
sasha0609 <oleksandralavr@gmail.com>
2024-04-14 23:18:46 +0200

    Before formatting

--- src/js/accordion/faq-accordion.js
+++ src/js/accordion/faq-accordion.js
@@ -1,36 +1,31 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-const section = document.querySelector('.container-ac');
+// Default options
+new Accordion('.accordion-container');
 
-const accordion = new Accordion('.container-ac', {
-  onOpen: function (target) {
-    const currentAnswer = target.querySelector('.answer-faq');
-
-    currentAnswer.classList.remove('is-hidden');
-  },
-  onClose: function (target) {
-    const currentAnswer = target.querySelector('.answer-faq');
-    currentAnswer.classList.add('is-hidden');
-  },
+// User options
+new Accordion('.ac', {
+  duration: 400,
+  showMultiple: true,
 });
 
-section.addEventListener('click', function (event) {
-  if (event.target.classList.contains('ac-trigger')) {
-    event.preventDefault();
-    const currentAnswer = event.target.parentNode.querySelector('.answer-faq');
-    if (currentAnswer.classList.contains('is-hidden')) {
-      currentAnswer.classList.remove('is-hidden');
-    } else {
-      currentAnswer.classList.add('is-hidden');
-    }
-  }
-  var btn = event.target;
-  const currentEl = btn;
-  if (~btn.className.indexOf(downClass)) {
-    btn.classList = currentEl.className.replace(downClass, upClass);
-  } else {
-    btn.classList = currentEl.className.replace(upClass, downClass);
-  }
-});
-var upClass = 'toggle-up';
-var downClass = 'toggle-down';
+// section.addEventListener('click', function (event) {
+//   // if (event.target.classList.contains('ac-trigger')) {
+//   //   event.preventDefault();
+//   //   const currentAnswer = event.target.parentNode.querySelector('.answer-faq');
+//   //   if (currentAnswer.classList.contains('is-hidden')) {
+//   //     currentAnswer.classList.remove('is-hidden');
+//   //   } else {
+//   //     currentAnswer.classList.add('is-hidden');
+//   //   }
+//   // }
+//   var btn = event.target;
+//   var upClass = 'toggle-up';
+//   var downClass = 'toggle-down';
+//   const currentEl = btn;
+//   if (~btn.className.includes(downClass)) {
+//     btn.classList = currentEl.className.replace(downClass, upClass);
+//   } else {
+//     btn.classList = currentEl.className.replace(upClass, downClass);
+//   }
+// });
