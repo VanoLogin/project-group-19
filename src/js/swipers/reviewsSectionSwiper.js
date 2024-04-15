c043bdb9ec15025cfbc3004ffed0956a08446db1
YaEhley <143018286+YaEhley@users.noreply.github.com>
2024-04-16 00:22:24 +0300

    small css changes

--- src/js/swipers/reviewsSectionSwiper.js
+++ src/js/swipers/reviewsSectionSwiper.js
@@ -7,13 +7,10 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 let reviewsSwiper;
 const reviewsListElem = document.querySelector('.reviews-swiper-wrapper');
-const prevButton = document.querySelector('.swiper-reviews-button-prev');
-const nextButton = document.querySelector('.swiper-reviews-button-next');
 
 initCardEditor();
 
 import getReviews  from '../interfaces/interfaceHttp';
-
 
 async function initCardEditor() {
     try {
@@ -22,16 +19,6 @@ async function initCardEditor() {
         reviewsListElem.insertAdjacentHTML('afterbegin', markUp);
         initSwiper()
 
-        // Set the initial state of the "Prev" button depending on the slider position
-        if (reviewsSwiper.isBeginning) {
-            prevButton.classList.add('disabled');
-            prevButton.setAttribute('aria-disabled', 'true');
-            prevButton.removeEventListener('click', navigatePrev);
-        } else {
-            prevButton.classList.remove('disabled');
-            prevButton.setAttribute('aria-disabled', 'false');
-            prevButton.addEventListener('click', navigatePrev);
-        }
     } catch {
             iziToast.error({
             position: 'topCenter',
@@ -40,7 +27,6 @@ async function initCardEditor() {
             });
         reviewsListElem.innerHTML = '<p class="not-found-message">Not found</p>';
         }
-
 }
 
 function initSwiper() {
@@ -71,42 +57,6 @@ function initSwiper() {
             }
         }
     });
-    reviewsSwiper.on('slideChange', updateNavigationButtons);
-}
-
-function updateNavigationButtons() {
-    // Check the current position of the slider
-    if (reviewsSwiper.isBeginning) {
-        // If the slider is on the first slide, tease out the "Prev" button
-        prevButton.classList.add('disabled');
-        prevButton.setAttribute('aria-disabled', 'true');
-        prevButton.removeEventListener('click', navigatePrev); // Remove the click event handler
-    } else {
-        // If the slider is not on the first slide, remove the disable and enable the "Prev" button
-        prevButton.classList.remove('disabled');
-        prevButton.setAttribute('aria-disabled', 'false');
-        prevButton.addEventListener('click', navigatePrev); // Add click event handler
-    }
-
-    if (reviewsSwiper.isEnd) {
-        // If the slider is on the last slide, tease out the "Next" button
-        nextButton.classList.add('disabled');
-        nextButton.setAttribute('aria-disabled', 'true');
-        nextButton.removeEventListener('click', navigateNext); // Remove the click event handler
-    } else {
-        // If the slider is not on the last slide, remove the disable and enable the "Next" button
-        nextButton.classList.remove('disabled');
-        nextButton.setAttribute('aria-disabled', 'false');
-        nextButton.addEventListener('click', navigateNext); // Add click event handler
-    }
-}
-
-function navigatePrev() {
-    reviewsSwiper.slidePrev();
-}
-
-function navigateNext() {
-    reviewsSwiper.slideNext();
 }
 
 window.addEventListener('keydown', tabNextSlide)
