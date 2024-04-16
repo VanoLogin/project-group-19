a64e87c97c09a82646050444d620a41dba17c1eb
sasha0609 <oleksandralavr@gmail.com>
2024-04-16 12:47:15 +0200

    add icon

--- src/js/accordion/faq-accordion.js
+++ src/js/accordion/faq-accordion.js
@@ -8,24 +8,3 @@ new Accordion('.ac', {
   duration: 400,
   showMultiple: true,
 });
-
-// section.addEventListener('click', function (event) {
-//   // if (event.target.classList.contains('ac-trigger')) {
-//   //   event.preventDefault();
-//   //   const currentAnswer = event.target.parentNode.querySelector('.answer-faq');
-//   //   if (currentAnswer.classList.contains('is-hidden')) {
-//   //     currentAnswer.classList.remove('is-hidden');
-//   //   } else {
-//   //     currentAnswer.classList.add('is-hidden');
-//   //   }
-//   // }
-//   var btn = event.target;
-//   var upClass = 'toggle-up';
-//   var downClass = 'toggle-down';
-//   const currentEl = btn;
-//   if (~btn.className.includes(downClass)) {
-//     btn.classList = currentEl.className.replace(downClass, upClass);
-//   } else {
-//     btn.classList = currentEl.className.replace(upClass, downClass);
-//   }
-// });
