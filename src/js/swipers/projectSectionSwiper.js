2eab5799b86d55128e6299aba0ddfd9b69af505e
Horbatko <142350622+Horbatko@users.noreply.github.com>
2024-04-17 22:08:00 +0300

    паа

--- src/js/swipers/projectSectionSwiper.js
+++ src/js/swipers/projectSectionSwiper.js
@@ -1,9 +1,13 @@
-
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const swiper = new Swiper('.project-swiper', {
   loop: false,
+  speed: 1000,
+
+  keyboard: {
+    enabled: true,
+},
 
   navigation: {
     nextEl: '.project-swiper-button-next',
