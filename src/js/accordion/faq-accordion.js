1ad0233682df6f2b316373d56e33e53434a0a209
Login_vano1234 <116017080+VanoLogin@users.noreply.github.com>
2024-04-18 18:00:55 +0300

    update_workTOgether

--- src/js/accordion/faq-accordion.js
+++ src/js/accordion/faq-accordion.js
@@ -1,10 +1,8 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-// Default options
-new Accordion('.accordion-container');
 
 // User options
-new Accordion('.ac', {
+new Accordion('.list-faq', {
   duration: 400,
   showMultiple: true,
 });
