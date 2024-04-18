d2002644c61711c404e2e0a243b691e6fdc5e07e
Login_vano1234 <116017080+VanoLogin@users.noreply.github.com>
2024-04-18 12:59:58 +0300

    update by ivan

---
+++ src/js/swipers/projectSectionSwiper.js
@@ -0,0 +1,21 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.project-swiper', {
+  loop: false,
+  speed: 1000,
+  grabCursor: true,
+  spaceBetween: 60, 
+
+  keyboard: {
+    enabled: true,
+},
+
+  navigation: {
+    nextEl: '.project-swiper-button-next',
+    prevEl: '.project-swiper-button-prev',
+  },
+
+
+
+});
