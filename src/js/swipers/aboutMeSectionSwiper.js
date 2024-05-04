35528a9cd0b822bfaa01c578b42a1600614730a7
Login_vano1234 <116017080+VanoLogin@users.noreply.github.com>
2024-05-04 13:23:32 +0300

    wellDone_04.05

--- src/js/swipers/aboutMeSectionSwiper.js
+++ src/js/swipers/aboutMeSectionSwiper.js
@@ -1,37 +1,54 @@
-import SwiperAbout from 'swiper';
-import { Navigation, Mousewheel, Keyboard } from 'swiper/modules';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+
+let parametrs = {
+  modules: [Navigation],
+  slidesPerView: 1,
+  lazy: true,
+  loop: true,
+  centeredSlides: false,
+  direction: 'horizontal',
+  spaceBetween: 0,
+  initialSlide: 1,
+  navigation: {
+    nextEl: '.about-me-swiper-btn-next',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 1,
+    // },
+    // when window width is >= 640px
+    500: {
+      slidesPerView: 3,
+    },
+    700: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+    1280: {
+      slidesPerView: 5,
+    },
+    1310: {
+      slidesPerView: 'auto',
+    },
+  },
+};
+
+new Swiper('.about-me-swiper-container', parametrs);
 
 new Accordion('.accordion-container', {
   duration: 600,
   showMultiple: false,
   openOnInit: [0],
 });
-
-let parametrs = {
-  modules: [Navigation, Mousewheel, Keyboard],
-  slidesPerView: 2,
-  lazy: true,
-  loop: true,
-  centeredSlides: false,
-  direction: 'horizontal',
-  mousewheel: true,
-  keyboard: true,
-  spaceBetween: 0,
-  initialSlide: 1,
-
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-    },
-    1440: {
-      slidesPerView: 6,
-    },
-  },
-
-  navigation: {
-    nextEl: '.about-me-swiper-btn-next',
-  },
-};
-
-new SwiperAbout('.about-me-swiper-container', parametrs);
